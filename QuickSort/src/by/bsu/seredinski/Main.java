f534e081e2ed0b395de0e4f495e55ed3d6cabfa3
TimSeredinski <rela1wer@gmail.com>
2018-09-27 11:13:39 +0300

    Changed average time counting

--- QuickSort/src/by/bsu/seredinski/Main.java
+++ QuickSort/src/by/bsu/seredinski/Main.java
@@ -7,8 +7,6 @@ import static by.bsu.seredinski.Algorithms.sort;
 
 public class Main {
     public static int n = 200;
-    private static long timeSpentHybrid;
-    private static long timeSpentQuickSort;
 
     public static void main(String[] args) throws IOException {
 
@@ -17,18 +15,30 @@ public class Main {
         while (n > 6) {
             int[][] mass1 = Array.readFromFile();
             int[][] mass2 = Array.readFromFile();
-            for (int i = 0; i < mass1.length; i++) {
+            long avgQuickSort = 0;
+            long avgHybridSort = 0;
+            for (int i = 0; i < mass2.length; i++) {
                 long startTimeQuickSort = System.nanoTime();
                 clearQuickSort(mass2[i], 0, mass2[i].length - 1);
-                timeSpentQuickSort = System.nanoTime() - startTimeQuickSort;
+                long timeSpentQuickSort = System.nanoTime() - startTimeQuickSort;
+                avgQuickSort += timeSpentQuickSort;
+            }
+
+            avgQuickSort /= mass2.length;
+
+            for (int i = 0; i < mass1.length; i++) {
                 long startTimeHybrid = System.nanoTime();
                 sort(mass1[i], 0, mass1[i].length - 1);
-                timeSpentHybrid = System.nanoTime() - startTimeHybrid;
+                long timeSpentHybrid = System.nanoTime() - startTimeHybrid;
+                avgHybridSort += timeSpentHybrid;
             }
-            if (timeSpentHybrid < timeSpentQuickSort) {
-                System.out.println("n = " + n + " гибридная сортировка быстрее на " + (timeSpentQuickSort - timeSpentHybrid) + " наносекунд");
+
+            avgHybridSort /= mass1.length;
+
+            if (avgHybridSort < avgQuickSort) {
+                System.out.println("n = " + n + " гибридная сортировка быстрее на " + (avgQuickSort - avgHybridSort) + " наносекунд");
             } else {
-                System.out.println("n = " + n + " " + (timeSpentHybrid - timeSpentQuickSort));
+                System.out.println("n = " + n + " " + (avgHybridSort - avgQuickSort));
             }
             n--;
         }
