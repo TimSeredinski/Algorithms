4bfb58ee46314fe90cbdd955500047a848b3e7a5
TimSeredinski <rela1wer@gmail.com>
2018-11-02 23:37:26 +0300

    Added components

--- Graphs/src/by/bsu/seredinski/algorithms/BFS.java
+++ Graphs/src/by/bsu/seredinski/algorithms/BFS.java
@@ -6,38 +6,39 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class BFS {
+    private static int amountOfComponents;
 
-    private static void setLevelsToTheVertices(Graph graph) {
+    private static void setLevelsToTheVertices(Graph graph, int vertex) {
         Queue<Integer> queue = new ArrayDeque<>();
-        int vertex = 0;
         queue.add(vertex);
         graph.setLevel(vertex, 0);
-
-        for (int i = 1; i < graph.getNumberOfVertices(); i++) {
-            graph.setLevel(i, -1);
-        }
-
+        amountOfComponents++;
+        System.out.println("Component " + amountOfComponents + ": ");
+        System.out.println("Level of vertex " + (vertex + 1) + " is equal " + 1);
         while (!queue.isEmpty()) {
-            System.out.println(queue);
             vertex = ((ArrayDeque<Integer>) queue).pop();
             for (int i = 0; i < graph.getConnectedVertices()[vertex].size(); i++) {
                 if (graph.getLevel(graph.getConnectedVertices()[vertex].get(i)) == -1) {
                     queue.add(graph.getConnectedVertices()[vertex].get(i));
                     graph.setLevel(graph.getConnectedVertices()[vertex].get(i), graph.getLevel(vertex) + 1);
+                    System.out.println("Level of vertex " + (graph.getConnectedVertices()[vertex].get(i) + 1) + " is equal " + (graph.getLevel(vertex) + 2));
                 }
             }
         }
     }
 
     public static void doBFS(Graph graph) {
-        setLevelsToTheVertices(graph);
-        printBFSLevels(graph);
-    }
-
-    private static void printBFSLevels(Graph graph) {
+        amountOfComponents = 0;
         for (int i = 0; i < graph.getNumberOfVertices(); i++) {
-            System.out.println("Level of vertex " + (i + 1) + " is equal " + (graph.getLevel(i) + 1));
+            graph.setLevel(i, -1);
+        }
+
+        for (int i = 0; i < graph.getNumberOfVertices(); i++) {
+            if (graph.getLevel(i) == -1) {
+                setLevelsToTheVertices(graph, i);
+            }
         }
     }
+
 
 }
