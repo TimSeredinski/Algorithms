1f08479e4c2f50c11efadce5872fb5eaa27b9a74
TimSeredinski <rela1wer@gmail.com>
2019-02-28 16:50:03 +0300

    Both tasks of Turing machine

--- Turing/src/by/bsu/seredinski/Main.java
+++ Turing/src/by/bsu/seredinski/Main.java
@@ -4,20 +4,45 @@ import java.util.Scanner;
 
 public class Main {
 
-    /*2.45 Считая непустое слово P записью положительного
+    public static void main(String[] args) {
+        /*2.45 Считая непустое слово P записью положительного
      троичного числа, уменьшить это число на 1.*/
 
-    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите положительное троичное число");
         String inputWord = scanner.nextLine();
         char[] str = ("#" + inputWord + "#").toCharArray();
         System.out.println(str);
-
-        String[][][] arrayOfConditions = {
-                {{"11+1"}, {"22+1"}, {"00+1"}, {"##-2"}},           //q1
-                {{"21-7"}, {"10-7"}, {"02-2"}, {"##-2"}}            //q2
+        String[][][] arrayOfConditions1 = {
+                {{"11+0"}, {"22+0"}, {"00+0"}, {"##-1"}},           //q1
+                {{"21-7"}, {"10-7"}, {"02-1"}, {"##-1"}}            //q2
         };
+        changeStringUsingConditions(str, arrayOfConditions1);
+
+        ////////////////////////////////////////////////////////////////
+
+        /*1.32Считая слово P записью числа в единичной системе счисления,
+     получить запись этого числа в троичной системе*/
+        System.out.println("Введите положительное число в единичной системе счисления, используя '|'");
+        inputWord = scanner.nextLine();
+        StringBuilder partOfStr = new StringBuilder();
+        for (int i = 0; i < inputWord.length(); i++) {
+            partOfStr.append("0");
+        }
+        str = ("#" + inputWord + "#" + partOfStr + "#").toCharArray();
+        System.out.println(str);
+        String[][][] arrayOfConditions2 = {
+                {{"|#+1"}, {"11+0"}, {"22+0"}, {"00+0"}, {"##+7"}},           //q0
+                {{"||+1"}, {"11+1"}, {"22+1"}, {"00+1"}, {"##+2"}},           //q1
+                {{"||+2"}, {"22+2"}, {"11+2"}, {"00+2"}, {"##-3"}},           //q2
+                {{"||+3"}, {"20-3"}, {"12-4"}, {"01-4"}, {"##-4"}},           //q3
+                {{"||+4"}, {"22-4"}, {"11-4"}, {"00-4"}, {"##-5"}},           //q4
+                {{"||-5"}, {"22-5"}, {"11-5"}, {"00-5"}, {"##+0"}},           //q5
+        };
+        changeStringUsingConditions(str, arrayOfConditions2);
+    }
+
+    private static void changeStringUsingConditions(char[] str, String[][][] arrayOfConditions) {
         int q = 0; //Номер текущего состояния
         for (int i = 1; i < str.length; ) {
             for (int j = 0; j < arrayOfConditions[0].length; j++) {
@@ -30,11 +55,11 @@ public class Main {
                     if (action[2] == '-') {
                         i--;
                     }
-                    q = Integer.parseInt(String.valueOf(action[3])) - 1;
+                    q = Integer.parseInt(String.valueOf(action[3]));
                     break;
                 }
             }
-            if (q == 6) {
+            if (q == 7) {
                 break;
             }
         }
